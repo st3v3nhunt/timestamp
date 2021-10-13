d6f44443a80e5dce4d7995dafd96628e72611554
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-10-13 10:39:31 +0100

    feat: add ability to return error responses

--- main.ts
+++ main.ts
@@ -1,5 +1,29 @@
+function generateErrorResponse(searchParams: URLSearchParams) {
+  const error = parseInt(searchParams.get("error") ?? "400", 10);
+  const errorRes = JSON.stringify({
+    error,
+  });
+  return new Response(errorRes, {
+    headers: { "content-type": "application/json; charset=UTF-8" },
+    status: error,
+  });
+}
+
 function handleRequest(request: Request): Response {
+  const url = new URL(request.url);
+  const path = url.pathname;
+
+  if (path.endsWith(".ico")) {
+    return new Response(null);
+  }
+
   console.log("Request:", request);
+
+  const searchParams = new URLSearchParams(url.searchParams);
+  if (searchParams.has("error")) {
+    return generateErrorResponse(searchParams);
+  }
+
   const now = Date.now();
   const res = JSON.stringify({
     now,
